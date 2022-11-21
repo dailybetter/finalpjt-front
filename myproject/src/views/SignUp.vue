63a2bd0f450df1d521dd72a77c2568640e46651b
bins95 <bins95@naver.com>
2022-11-21 10:47:41 +0900

    feat:signup

--- myproject/src/views/SignUp.vue
+++ myproject/src/views/SignUp.vue
@@ -7,13 +7,13 @@
 
       <div class="stepper-item current" :class="{'current': step == item, 'success': step>item}" v-for="item in 4" :key="item" >
         <div class="stepper-item-counter">
-          <img src="@/assets/icons8-check-mark-48.png" alt="" class="icon-success">
+          <img src="@/assets/check-mark.png" alt="" class="icon-success">
           <span class="number">
             {{item}}
           </span>
         </div>
         <span class="stepper-item-title">
-          step {{step}}
+          step {{item}}
         </span>
       </div>
     </div>
@@ -22,11 +22,23 @@
         container입니다<span class="tx-green-1">{{step}}</span>번째
       </div>
     </div>
+    <div class="stepper-content">
+      <div class="stepper-complete" v-if="step == 5" :disabled="step<5">
+        <img class= "stepper-complete stepper-complete--img" src="@/assets/welcomeEmoji.gif" alt="">
+        <button class="btn btn--complete" @click="goMain" :disabled="step<5">
+        Go
+        </button>
+      </div>
+
+    </div>
     <div class="controls">
-      <button class="btn" @click="step--" :disabled="step == 1">
+      <button class="btn" @click="step--" :disabled="step == 1 ||step == 5">
         Before
       </button>
-      <button class="btn btn--green-1" @click="nextbtn" :disabled="step == 4">
+      <button class="btn btn--complete" @click="step++" :disabled="step<4 || step == 5">
+        Complete
+      </button>
+      <button class="btn btn--green-1" @click="step++" :disabled="step == 4 || step == 5">
         Next
       </button>
     </div>
@@ -43,14 +55,16 @@ export default {
   },
   computed: {
     stepperProgress() {
+      if(this.step == 5){
+        return 100 +'%'
+      } else{
       return (100/3)*(this.step - 1) + '%'
+      }
     }
   },
   methods: {
-    nextbtn() {
-      this.step++
-      console.log('되긴됨')
-      console.log(this.step)
+    goMain(){
+      console.log('gomain')
     }
   }
 
